40715a02c47d27ce42ba84254b6080fb2008e3b7
wilfreddenton <dentonw3@gmail.com>
2018-12-26 11:20:33 -0500

    added Pure no logging MonadLogger instance

--- src/Lib/Effects/Logger.hs
+++ src/Lib/Effects/Logger.hs
@@ -20,23 +20,33 @@ class Monad m => MonadLogger m where
 
 -- Implementations
 
+-- Pure
+logPure :: Monad m => Text -> m ()
+logPure _ = pure ()
+
+withNamespacePure :: Text -> m a -> m a
+withNamespacePure _ action = action
+
+withContextPure :: ToJSON b => b -> m a -> m a
+withContextPure context action = let _ = toJSON context in action
+
 -- Katip
-logKatip :: (KatipContext m) => Severity -> Text -> m ()
+logKatip :: KatipContext m => Severity -> Text -> m ()
 logKatip severity = $(logTM) severity . logStr
 
-debugKatip :: (KatipContext m) => Text -> m ()
+debugKatip :: KatipContext m => Text -> m ()
 debugKatip = logKatip DebugS
 
-errorKatip :: (KatipContext m) => Text -> m ()
+errorKatip :: KatipContext m => Text -> m ()
 errorKatip = logKatip ErrorS
 
-infoKatip :: (KatipContext m) => Text -> m ()
+infoKatip :: KatipContext m => Text -> m ()
 infoKatip = logKatip InfoS
 
-warnKatip :: (KatipContext m) => Text -> m ()
+warnKatip :: KatipContext m => Text -> m ()
 warnKatip = logKatip WarningS
 
-withNamespaceKatip :: (KatipContext m) => Text -> m a -> m a
+withNamespaceKatip :: KatipContext m => Text -> m a -> m a
 withNamespaceKatip namespace = katipAddNamespace (Namespace [namespace])
 
 withContextKatip :: (KatipContext m, ToJSON b) => b -> m a -> m a
