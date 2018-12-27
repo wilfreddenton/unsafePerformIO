3e2fb639f2691dfede8a0ab8b3d7137347c78e26
wilfreddenton <dentonw3@gmail.com>
2018-12-26 22:05:25 -0500

    integrated lucid html dsl

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,10 @@
-{-# LANGUAGE DataKinds      #-}
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE DeriveGeneric  #-}
-{-# LANGUAGE TypeOperators  #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE DeriveAnyClass    #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE TypeOperators     #-}
 
 module Lib (
   app
@@ -14,15 +17,31 @@ import           Lib.App            (App, appToHandler)
 import           Lib.Effects.Logger (MonadLogger, info, withContext,
                                      withNamespace)
 import           Lib.Env            (AppEnv)
+import           Lucid              (ToHtml, class_, div_, h1_, h3_, li_, p_,
+                                     toHtml, toHtmlRaw, ul_)
 import           Network.Wai        (Application)
 import           Protolude          hiding (log)
 import           Servant            ((:>), Get, JSON, ServerT, hoistServer,
                                      serve)
+import           Servant.HTML.Lucid (HTML)
 
 data Post = Post {
   title :: Text
 , body  :: Text
 } deriving (Eq, Show, Generic, ToJSON, FromJSON)
+
+instance ToHtml Post where
+  toHtmlRaw = toHtml
+  toHtml Post{..} = div_ [class_ "post"] $ do
+    h1_ $ toHtml title
+    p_ $ toHtml body
+
+instance ToHtml [Post] where
+  toHtmlRaw = toHtml
+  toHtml = ul_ . foldMap asListItem
+    where
+      asListItem post = li_ $ do
+        h3_ . toHtml $ title post
 
 posts :: [Post]
 posts = [ Post "Hello, World!" "blah blah blah"
@@ -38,7 +57,7 @@ getPosts = withNamespace "getPosts" $ do
   subHandler
   pure posts
 
-type API = "posts" :> Get '[JSON] [Post]
+type API = "posts" :> Get '[JSON, HTML] [Post]
 
 serverT :: ServerT API App
 serverT = getPosts
