16c6acecef67f703cf3dfc9648d5606f01341b74
wilfreddenton <dentonw3@gmail.com>
2018-12-28 18:18:20 -0500

    added fonts; integrated bootstrap and started working on UI layout

--- src/Lib/Effects/Post.hs
+++ src/Lib/Effects/Post.hs
@@ -8,8 +8,8 @@ module Lib.Effects.Post where
 
 import           Data.Aeson     (FromJSON, ToJSON)
 import           Lib.Orphans    ()
-import           Lucid.Extended (ToHtml, class_, div_, h1_, h3_, li_, p_,
-                                 toHtml, toHtmlRaw, ul_)
+import           Lucid.Extended (ToHtml, class_, div_, h1_, li_, p_, toHtml,
+                                 toHtmlRaw, ul_)
 import           Protolude
 import qualified Text.MMark     as MMark
 
@@ -34,7 +34,8 @@ instance ToHtml [Post] where
   toHtml = ul_ . foldMap asListItem
     where
       asListItem post = li_ $ do
-        h3_ . toHtml $ title post
+        toHtml post
+        -- h3_ . toHtml $ title post
 
 -- Typeclass
 class Monad m => MonadPost m where
@@ -45,6 +46,7 @@ class Monad m => MonadPost m where
 -- Pure
 getPostsPure :: Monad m => m [Post]
 getPostsPure = pure $
-  [ Post "Hello, World!" "```haskell\nid :: a -> a\nid a = a\n```"
-  , Post "Foo" "Bar"
+  [ Post "Hello, World!" "Here is a snippet of code bro.\n```haskell\ninstance ToHtml Post where\n  toHtmlRaw = toHtml\n  toHtml Post{..} = div_ [class_ \"post\"] $ do\n    h1_ $ toHtml title\n    markdown\n    where\n      markdown = toHtml $ case MMark.parse (show title) body of\n        Left _  -> p_ \"invalid markdown\" -- should never run\n        Right m -> MMark.render m\n```\nDid you enjoy this code?"
+  , Post "Foo" "Let's go to the *Bar* ok?"
+  , Post "Welcome to the Blog" "*unsafePerformIO*"
   ]
--- src/Lucid/Extended.hs
+++ src/Lucid/Extended.hs
@@ -7,6 +7,18 @@ import           Data.Aeson (ToJSON, toJSON)
 import           Lucid
 import           Protolude
 
+container_ :: Term arg result => arg -> result
+container_ = termWith "div" [class_ " container "]
+
+row_ :: Term arg result => arg -> result
+row_ = termWith "div" [class_ " row "]
+
+colMd4_ :: Term arg result => arg -> result
+colMd4_ = termWith "div" [class_ " col-md-4 "]
+
+colMd8_ :: Term arg result => arg -> result
+colMd8_ = termWith "div" [class_ " col-md-8 "]
+
 data Template a = Template Text a
 
 instance ToJSON a => ToJSON (Template a) where
@@ -18,6 +30,14 @@ instance ToHtml a => ToHtml (Template a) where
     head_ $ do
       title_ $ toHtml title
       meta_ [charset_ "utf-8"]
-      link_ [rel_ "stylesheet", type_ "text/css", href_ "/static/style.css"]
-      script_ [src_ "/static/script.js"] ("" :: Text)
-    body_ $ toHtml a
+      link_ [rel_ "stylesheet", type_ "text/css", href_ "/static/css/style.css"]
+      link_ [rel_ "stylesheet", type_ "text/css", href_ "/static/css/github.css"]
+      link_ [rel_ "stylesheet", type_ "text/css", href_ "/static/css/bootstrap-grid.min.css"]
+      script_ [src_ "/static/js/script.js"] ("" :: Text)
+      script_ [src_ "/static/js/highlight.pack.js"] ("" :: Text)
+      script_ "hljs.initHighlightingOnLoad()"
+    body_ . container_ . row_ $ do
+      colMd4_ . nav_ $ do
+        h2_ [id_ "title"] "unsafePerformIO"
+        p_ "[ Author: Wilfred Denton ]"
+      colMd8_ . div_ [class_ "content"] $ toHtml a
