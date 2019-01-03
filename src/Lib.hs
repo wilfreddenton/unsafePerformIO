c2751c1eeba1e93f6026359458a6b40abc28d5d3
wilfreddenton <dentonw3@gmail.com>
2019-01-03 18:04:03 -0500

    added global request throttling

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,16 +1,22 @@
 module Lib where
 
-import           Control.Lens             ((^.))
-import           Database.SQLite.Simple   (close)
-import           Katip                    (closeScribes)
-import           Lib.App                  (runLoggerT)
-import           Lib.Effects.Logger       (infoKatip, withContextKatip)
-import           Lib.Env                  (AppEnv (AppEnv), ServerEnv, dConn,
-                                           lLogEnv, newDbEnv, newLoggerEnv,
-                                           sPort, sSqliteDatabase, serverEnv)
-import           Lib.Server               (app)
-import           Network.Wai.Handler.Warp (run)
+import           Control.Lens                    ((^.))
+import           Database.SQLite.Simple          (close)
+import           Katip                           (closeScribes)
+import           Lib.App                         (runLoggerT)
+import           Lib.Effects.Logger              (infoKatip, withContextKatip)
+import           Lib.Env                         (AppEnv (AppEnv), ServerEnv,
+                                                  dConn, lLogEnv, newDbEnv,
+                                                  newLoggerEnv, sPort,
+                                                  sSqliteDatabase, serverEnv)
+import           Lib.Server                      (app)
+import           Network.Wai.Handler.Warp        (run)
+import           Network.Wai.Middleware.Throttle (defaultThrottleSettings,
+                                                  initThrottler, throttle,
+                                                  throttleSettingsBurst,
+                                                  throttleSettingsRate)
 import           Protolude
+import           System.Clock                    (TimeSpec (TimeSpec))
 
 initialize :: ServerEnv -> IO ()
 initialize serverEnv' = bracket makeAppEnv stopApp runApp
@@ -21,8 +27,10 @@ initialize serverEnv' = bracket makeAppEnv stopApp runApp
       pure $ AppEnv serverEnv' loggerEnv dbEnv
     runApp env = do
       let port = env^.sPort
+      throttler <- initThrottler (defaultThrottleSettings $ TimeSpec 5 0) { throttleSettingsRate = 15, throttleSettingsBurst = 20 }
+      let f = throttle throttler
       liftIO . runLoggerT env . withContextKatip (env^.serverEnv) $ infoKatip "Running"
-      run port $ app env
+      run port . f $ app env
     stopApp env = do
       liftIO . runLoggerT env $ infoKatip "Shutting down gracefully"
       liftIO . close $ env^.dConn
