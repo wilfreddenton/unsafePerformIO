ea4a17a1a4fe6e6f54d66ad8b336769fb06d8c89
wilfreddenton <dentonw3@gmail.com>
2019-01-02 23:18:04 -0500

    working on adding pgp authentication

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,16 @@
 module Main where
 
 import           Cli                 (opts)
+import           Crypto.Gpgme
 import           Lib                 (initialize)
 import           Options.Applicative (execParser)
+import           Prelude             (String)
 import           Protolude
 
 
 main :: IO ()
-main = initialize =<< execParser opts
+main = do
+  withCtx "/home/wilfred/Documents/haskell-starter-pack/.gnupg" "C" OpenPGP $ \ctx -> do
+    ks <- listKeys ctx NoSecret
+    putStrLn $ (show $ length ks :: String)
+  initialize =<< execParser opts
