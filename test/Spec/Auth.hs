2e88882b5e93207adfd3d2de2b8e79bf5c49883e
wilfreddenton <dentonw3@gmail.com>
2019-01-07 01:07:02 -0500

    started using quick check property tests for auth spec

--- test/Spec/Auth.hs
+++ test/Spec/Auth.hs
@@ -5,16 +5,30 @@ module Spec.Auth (
   authSpec
 ) where
 
-import           Control.Lens       (makeClassy)
-import           Lib.Effects.Auth   (MonadAuth, authorize, authorizeIO)
-import           Lib.Effects.Logger (MonadLogger, debug, error, info, logPure,
-                                     warn, withContext, withContextPure,
-                                     withNamespace, withNamespacePure)
-import           Lib.Env            (AuthEnv, HasAuthEnv, authEnv, newAuthEnv)
-import           Lib.Error          (ApiError (UnauthorizedError),
-                                     AppError (AppApiError))
+import           Control.Lens                   (makeClassy, view, ( # ))
+import           Crypto.Gpgme                   (SignMode (Detach), setArmor,
+                                                 setPassphraseCallback, sign)
+import qualified Data.Text.Encoding             as T
+import           Lib.Effects.Auth               (MonadAuth, authorize,
+                                                 authorizeIO)
+import           Lib.Effects.Logger             (MonadLogger, debug, error,
+                                                 info, logPure, warn,
+                                                 withContext, withContextPure,
+                                                 withNamespace,
+                                                 withNamespacePure)
+import           Lib.Env                        (AuthEnv, HasAuthEnv, aCtx,
+                                                 authEnv, newAuthEnv)
+import           Lib.Error                      (ApiError (UnauthorizedError),
+                                                 AppError (AppApiError),
+                                                 logAndThrow,
+                                                 _AuthorizationFailedError)
 import           Protolude
-import           Test.Tasty.Hspec   (Spec, describe, it, shouldReturn)
+import           Servant                        (NoContent (NoContent))
+import           Test.Hspec.Core.QuickCheck     (modifyMaxSuccess)
+import           Test.QuickCheck                (property)
+import           Test.QuickCheck.Instances.Text ()
+import           Test.Tasty.Hspec               (Spec, describe, it,
+                                                 shouldReturn)
 
 data MockAppEnv = MockAppEnv {
   _mockAppAuthEnv :: AuthEnv
@@ -46,6 +60,19 @@ runMockApp action = do
 
 authSpec :: Spec
 authSpec = do
-  describe "Auth" $
-    it "does not authoriza invalid signature" $
+  describe "Auth" $ do
+    it "does not authorize a invalid signature" $
       runMockApp (authorize "" "") `shouldReturn` Left (AppApiError UnauthorizedError)
+    modifyMaxSuccess (const 20) . it "blah" . property $ \clearText -> do
+      let clearTextBs = T.encodeUtf8 clearText
+          action = do
+            ctx <- view aCtx
+            liftIO $ setArmor True ctx
+            liftIO $ setPassphraseCallback ctx (Just $ \_ _ _ -> pure $ Just "password")
+            result <- liftIO $ sign ctx [] Detach clearTextBs
+            detachedSignature <- case result of
+              Right sig -> pure sig
+              _         -> logAndThrow $ _AuthorizationFailedError # ""
+            authorize (T.decodeUtf8 detachedSignature) clearText
+            pure NoContent
+      runMockApp action `shouldReturn` Right NoContent
