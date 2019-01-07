9bb0c326bf4328f4c64557d431d7c0dc7783a6f2
wilfreddenton <dentonw3@gmail.com>
2019-01-06 19:12:08 -0500

    working on auth spec

--- test/Spec/Auth.hs
+++ test/Spec/Auth.hs
@@ -1,12 +1,51 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TemplateHaskell            #-}
+
 module Spec.Auth (
   authSpec
 ) where
 
+import           Control.Lens       (makeClassy)
+import           Lib.Effects.Auth   (MonadAuth, authorize, authorizeIO)
+import           Lib.Effects.Logger (MonadLogger, debug, error, info, logPure,
+                                     warn, withContext, withContextPure,
+                                     withNamespace, withNamespacePure)
+import           Lib.Env            (AuthEnv, HasAuthEnv, authEnv, newAuthEnv)
+import           Lib.Error          (ApiError (UnauthorizedError),
+                                     AppError (AppApiError))
 import           Protolude
-import           Test.Tasty.Hspec (Spec, describe, it, shouldReturn)
+import           Test.Tasty.Hspec   (Spec, describe, it, shouldReturn)
+
+data MockAppEnv = MockAppEnv {
+  _mockAppAuthEnv :: AuthEnv
+}
+makeClassy ''MockAppEnv
+
+instance HasAuthEnv MockAppEnv where
+  authEnv = mockAppAuthEnv . authEnv
+
+newtype MockApp a = MockApp {
+  unMockApp :: ReaderT MockAppEnv (ExceptT AppError IO) a
+} deriving (Monad, Functor, Applicative, MonadReader MockAppEnv, MonadError AppError, MonadIO)
+
+instance MonadAuth MockApp where
+  authorize = authorizeIO
+
+instance MonadLogger MockApp where
+  debug = logPure
+  error = logPure
+  info = logPure
+  warn = logPure
+  withNamespace = withNamespacePure
+  withContext = withContextPure
+
+runMockApp :: MockApp a -> IO (Either AppError a)
+runMockApp action = do
+  env <- MockAppEnv <$> newAuthEnv ".gnupg-test" "wilfred.gpg"
+  runExceptT . flip runReaderT env $ unMockApp action
 
 authSpec :: Spec
 authSpec = do
   describe "Auth" $
-    it "returns blah" $
-      pure (1 :: Int) `shouldReturn` (1 :: Int)
+    it "does not authoriza invalid signature" $
+      runMockApp (authorize "" "") `shouldReturn` Left (AppApiError UnauthorizedError)
