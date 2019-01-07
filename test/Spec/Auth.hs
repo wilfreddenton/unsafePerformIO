ba07fbc3655a00302004b6cfdeb42ada8c9742c5
wilfreddenton <dentonw3@gmail.com>
2019-01-07 11:03:27 -0500

    finished auth spec

--- test/Spec/Auth.hs
+++ test/Spec/Auth.hs
@@ -6,7 +6,9 @@ module Spec.Auth (
 ) where
 
 import           Control.Lens                   (makeClassy, view, ( # ))
-import           Crypto.Gpgme                   (SignMode (Detach), setArmor,
+import           Crypto.Gpgme                   (Ctx, Protocol (OpenPGP),
+                                                 SignMode (Detach), newCtx,
+                                                 setArmor,
                                                  setPassphraseCallback, sign)
 import qualified Data.Text.Encoding             as T
 import           Lib.Effects.Auth               (MonadAuth, authorize,
@@ -20,14 +22,14 @@ import           Lib.Env                        (AuthEnv, HasAuthEnv, aCtx,
                                                  authEnv, newAuthEnv)
 import           Lib.Error                      (ApiError (UnauthorizedError),
                                                  AppError (AppApiError),
-                                                 logAndThrow,
+                                                 CanApiError, logAndThrow,
                                                  _AuthorizationFailedError)
 import           Protolude
 import           Servant                        (NoContent (NoContent))
 import           Test.Hspec.Core.QuickCheck     (modifyMaxSuccess)
 import           Test.QuickCheck                (property)
 import           Test.QuickCheck.Instances.Text ()
-import           Test.Tasty.Hspec               (Spec, describe, it,
+import           Test.Tasty.Hspec               (Spec, describe, it, runIO,
                                                  shouldReturn)
 
 data MockAppEnv = MockAppEnv {
@@ -53,26 +55,46 @@ instance MonadLogger MockApp where
   withNamespace = withNamespacePure
   withContext = withContextPure
 
+aliceHomedir :: FilePath
+aliceHomedir = ".gnupg-alice"
+
+bobHomedir :: FilePath
+bobHomedir = ".gnupg-bob"
+
 runMockApp :: MockApp a -> IO (Either AppError a)
 runMockApp action = do
-  env <- MockAppEnv <$> newAuthEnv ".gnupg-test" "wilfred.gpg"
+  env <- MockAppEnv <$> newAuthEnv aliceHomedir "wilfred.gpg"
   runExceptT . flip runReaderT env $ unMockApp action
+
+makeSignature :: (MonadLogger m, CanApiError e m, MonadIO m) => Ctx -> Text -> m Text
+makeSignature ctx clearText = do
+  let clearTextBs = T.encodeUtf8 clearText
+  liftIO $ setArmor True ctx
+  liftIO $ setPassphraseCallback ctx (Just $ \_ _ _ -> pure $ Just "password")
+  result <- liftIO $ sign ctx [] Detach clearTextBs
+  case result of
+    Right sig -> pure $ T.decodeUtf8 sig
+    _         -> logAndThrow $ _AuthorizationFailedError # ""
 
 authSpec :: Spec
 authSpec = do
+  let n = 20
+      failure = Left $ AppApiError UnauthorizedError
   describe "Auth" $ do
-    it "does not authorize a invalid signature" $
-      runMockApp (authorize "" "") `shouldReturn` Left (AppApiError UnauthorizedError)
-    modifyMaxSuccess (const 20) . it "blah" . property $ \clearText -> do
-      let clearTextBs = T.encodeUtf8 clearText
-          action = do
+    it "does not authorize random signatures and cleartext" . property $ \sig clearText -> do
+      runMockApp (authorize sig clearText) `shouldReturn` failure
+
+    bobCtx <- runIO $ newCtx bobHomedir "C" OpenPGP
+    modifyMaxSuccess (const n) . it "does not authorize a invalid signature" . property $ \clearText -> do
+      let action = do
+            sig <- makeSignature bobCtx clearText
+            authorize sig clearText
+      runMockApp action `shouldReturn` failure
+
+    modifyMaxSuccess (const n) . it "does authorize valid signatures" . property $ \clearText -> do
+      let action = do
             ctx <- view aCtx
-            liftIO $ setArmor True ctx
-            liftIO $ setPassphraseCallback ctx (Just $ \_ _ _ -> pure $ Just "password")
-            result <- liftIO $ sign ctx [] Detach clearTextBs
-            detachedSignature <- case result of
-              Right sig -> pure sig
-              _         -> logAndThrow $ _AuthorizationFailedError # ""
-            authorize (T.decodeUtf8 detachedSignature) clearText
+            sig <- makeSignature ctx clearText
+            authorize sig clearText
             pure NoContent
       runMockApp action `shouldReturn` Right NoContent
