694ff42c7ff186b8fb05b7ff1a6acf97a1db82f8
wilfreddenton <dentonw3@gmail.com>
2020-01-16 16:48:52 +0000

    cleanup script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,22 @@
 function main() {
-  pres = document.querySelectorAll('pre');
-  test = document.querySelector('#asdf');
-  pres.forEach((pre) => {
-    var code = pre.querySelector('code');
+  document.querySelectorAll('pre').forEach(pre => {
+    const code = pre.querySelector('code')
     if (code) {
-      var frag = document.createDocumentFragment();
-      lines = code.innerHTML.trim().split('\n');
-      lines.forEach((line) => {
-        el = document.createElement('code');
-        el.innerHTML = line;
-        frag.append(el);
-      });
-      pre.removeChild(code);
-      pre.appendChild(frag);
+      const frag = document.createDocumentFragment()
+      const lines = code.innerHTML.trim().split('\n')
+      lines.forEach(line => {
+        const el = document.createElement('code')
+        el.innerHTML = line
+        frag.append(el)
+      })
+      pre.removeChild(code)
+      pre.appendChild(frag)
     }
-  });
+  })
 }
 
 if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", main);
+  document.addEventListener("DOMContentLoaded", main)
 } else {
-  main();
+  main()
 }
